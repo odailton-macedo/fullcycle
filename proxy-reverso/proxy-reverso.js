79ff9a64dc2018b7b5e53191508abc058aa7f797
Odailton Macedo <odailton.macedo@yahoo.com.br>
2023-11-10 10:48:16 -0300

    Criação de tabela e adição do Dockerize na aplicação.

--- proxy-reverso/proxy-reverso.js
+++ proxy-reverso/proxy-reverso.js
@@ -1,10 +1,10 @@
-//////////////////////////////
-// Desafio Proxy Reverso    //
-// Nginx >> Nodejs >> MySQL // 
-//////////////////////////////
+/************************************************
+** Desafio Proxy Reverso                       **
+** Nginx >> Nodejs >> MySQL                    **
+************************************************/
 
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
 const app = express();
 const port = 3000;
@@ -18,56 +18,50 @@ const dbConfig = {
 };
 
 // Middleware para conectar ao MySQL antes de lidar com as requisições
-app.use((req, res, next) => {
-  const db = mysql.createConnection(dbConfig);
-
-  db.connect(err => {
-    if (err) {
-      const msg = 'O servidor de banco de dados não está disponível.'
-      console.error(msg  + '\n', err);
-      return res.status(500).json({ error: msg });
+app.use(async (req, res, next) => {
+  try {
+    // Conexão com o banco de dados
+    failure_log = 'Falha na conexão com o banco de dados.';
+    const db = await mysql.createConnection(dbConfig);    
+    
+    // Verifica se a tabela "people" existe
+    failure_log = 'Falha ao verificar a existência da tabela "people".';
+    const [rows] = await db.execute(`SELECT COUNT(1) as count FROM information_schema.tables WHERE table_schema = '${dbConfig.database}' AND table_name = 'people'`);
+    const tableExists = rows[0].count > 0;  
+    
+    // Se a tabela não existir, cria a tabela
+    if (!tableExists) {
+      failure_log = 'Falha na criação da tabela "people".';
+      await db.execute("CREATE TABLE people(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))");
+      console.log('A tabela "people" foi criada.');
     }
 
-    // Adicione o objeto de conexão ao pedido para que possa ser usado nas rotas
-    req.db = db;
+    // Insert na tabela "people"
+    failure_log = 'Falha ao inserir registro na tabela "people".';
+    const query = `INSERT INTO people (name) values('${generateRandomName()}')`;
+    db.execute(query);
 
-    next();
-  });
+    // Lista tabela "people"
+    failure_log = 'Falha ao buscar registros na tabela "people".';
+    const [peoples] = await db.execute('SELECT id, name FROM people ORDER BY id DESC');
+    
+    // Formata a exibição em HTML
+    const html = tableHTML(peoples);
+    
+    // Retorna o HTML
+    res.send(html);
+
+    await db.end();
+  }
+  catch(err) {
+    console.error(failure_log  + '\n', err);
+    return res.status(500).json({ err: failure_log });
+  }
 });
 
-
-// Responde à rota "/"
-app.get('/', (req, res) => {
-  insertPeople(req, res);
-  listPeople(req, res);
-  req.db.end();
-});
-
-
-// Insert na tabela "people"
-function insertPeople(req, res) {
-  const query = `INSERT INTO people (name) values('${generateRandomName()}')`;
-  req.db.query(query, (err, results) => {
-    if (err) {
-      console.error('Erro ao inserir pessoa no banco de dados: ', err);
-      res.status(500).send('Erro ao inserir pessoa no banco de dados');
-      return;
-    }
-  });
-}
-
-// Lista a tabela "people"
-function listPeople(req, res) {
-  const query = 'SELECT id, name FROM people ORDER BY id DESC';  
-  req.db.query(query, (err, results) => {
-    if (err) {
-      console.error('Erro ao consultar o banco de dados: ', err);
-      res.status(500).send('Erro ao consultar o banco de dados');
-      return;
-    }
-
-    // Converte resultados em uma tabela HTML
-    const tableHTML = `
+// Formata a tabela "people" para exibição em HTML
+function tableHTML(rows) {
+    const html = `
       <html>
       <head>
         <style>
@@ -94,14 +88,13 @@ function listPeople(req, res) {
             <th>ID</th>
             <th>Name</th>
           </tr>
-          ${results.map(row => `<tr><td>${row.id}</td><td>${row.name}</td></tr>`).join('')}
+          ${rows.map(row => `<tr><td>${row.id}</td><td>${row.name}</td></tr>`).join('')}
         </table>
       </body>
       </html>
     `;
 
-    res.send(tableHTML);
-  });
+    return html;
 }
 
 app.listen(port, () => {
@@ -115,7 +108,7 @@ function getRandomElement(arr) {
 
 function generateRandomName() {
   const firstnames = ["David", "Jimi", "Albert", "Robert", "Dave", "Jim", "Mick", "Aretha", "Janis", "Natalie", "Dolores"];
-  const lastnames = ["Gilmour", "Hendrix", "King", "Smith", "Gahan", "Morrison", "Jagger", "Franklin", "Joplin", "Merchant", "O´ Riordan"];
+  const lastnames  = ["Gilmour", "Hendrix", "King", "Smith", "Gahan", "Morrison", "Jagger", "Franklin", "Joplin", "Merchant", "O´ Riordan"];
 
   const randomFirstname = getRandomElement(firstnames);
   const randomLastname = getRandomElement(lastnames);
